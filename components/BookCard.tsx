fe4220ec30257673a91c039bb2e418b28a4cb638
yy933 <116449560+yy933@users.noreply.github.com>
2026-08-14 13:17:18 +0800

    feat: update BookCard component

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
 import { FaRegHeart } from "react-icons/fa6";
 import Pill from "./Pill";
 import { BookCardProps } from "@/app/types";
-import placeholderImg from "@/public/placeholder.jpg";
+import placeholderImg from "@/public/placeholder-img.jpg";
 
 export default function BookCard({ book }: BookCardProps) {
   return (
     <Link
-      href={`/3d-models/${book.id}`}
+      href={`/books/${book.id}`}
       className="block group hover:shadow-[0_5px_12px_rgba(0,0,0,0.1)] hover:-translate-y-[3px] transition-all"
       aria-labelledby={`book-${book.id}-title`}
     >
@@ -16,24 +17,26 @@ export default function BookCard({ book }: BookCardProps) {
         role="article"
       >
         <div className="relative aspect-square">
-          <img
-            src={placeholderImg.src}
+          <Image
+            src={placeholderImg}
             alt={book.name}
-            className="absolute inset-0 object-cover w-full h-full"
+            fill
+            className="object-center object-cover"
+            sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
           />
         </div>
         <div className="p-4">
           <div className="flex justify-between mb-2 min-h-[3.5rem]">
             <h2
               id={`book-${book.id}-title`}
-              className="text-xl font-semibold text-gray-800 line-clamp-2"
+              className="text-xl font-bold text-gray-800 line-clamp-2"
             >
               {book.name}
             </h2>
           </div>
-          <p className="text-gray-800 text-sm line-clamp-2 min-h-[2.5rem] leading-[1.25rem]">
-            {book.description}
-          </p>
+          <span className="text-gray-800 text-lg font-semibold line-clamp-2 min-h-[2.5rem] leading-[1.25rem]">
+            {book.author}
+          </span>
           <div className="mt-2">
             <Pill>{book.category}</Pill>
           </div>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "place-hold.it",
+        port: "",
+        pathname: "/**",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
