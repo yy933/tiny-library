e58476079717449a6b6dac07d6f07352121fac85
yy933 <116449560+yy933@users.noreply.github.com>
2026-08-14 12:59:36 +0800

    feat: add getBooks function and implement it in /books page to get book data

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,3 +1,22 @@
-export default function BooksPage(){
-  return (<h1>This is books page</h1>)
+import BooksGrid from "@/components/BooksGrid";
+import Form from "next/form";
+import { getBooks } from "@/lib/utils";
+export default function BooksPage() {
+  const filteredBooks = getBooks(); 
+
+  return (
+    <>
+      <Form action="/3d-models" className="w-full px-4 md:max-w-xl">
+        <input
+          type="search"
+          name="q"
+          placeholder="Search 3D models..."
+          autoComplete="off"
+          // defaultValue={q}
+          className="w-full py-3 pl-5 pr-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none focus:ring-0 md:text-base"
+        ></input>
+      </Form>
+      <BooksGrid title="Books" books={filteredBooks} />
+    </>
+  );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import BooksData from "@/app/data/books.json";
+import type { Book } from "@/app/types";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getBooks({
+  category,
+}: { category?: string } = {}): Book[] {
+  if (!category) return BooksData;
+
+  return BooksData.filter((book: Book) => book.category === category);
 }
